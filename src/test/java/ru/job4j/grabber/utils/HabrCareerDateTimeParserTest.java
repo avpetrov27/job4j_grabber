9a7f8b654c28736c099e66e4d284b1b98b985fc1
avpetrov27 <avpetrov27@yandex.ru>
2024-06-07 00:29:55 +0300

    2.1. Преобразование даты [#289476]

---
+++ src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.grabber.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.assertj.core.api.Assertions.*;
+
+class HabrCareerDateTimeParserTest {
+    @Test
+    void whenParse() {
+        LocalDateTime date0 = LocalDateTime.now();
+        String text = date0.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        assertThat(new HabrCareerDateTimeParser().parse(text)).isEqualTo(date0);
+    }
+}
