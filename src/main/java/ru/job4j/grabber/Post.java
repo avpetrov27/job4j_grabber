c7b95d8563feef1bfc4d45ead611d50e417aaf91
avpetrov27 <avpetrov27@yandex.ru>
2024-06-24 22:09:48 +0300

    2.2. Модель данных - Post. [#285211 #502231]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -78,11 +78,11 @@ public class Post {
             return false;
         }
         Post post = (Post) o;
-        return Objects.equals(link, post.link);
+        return id == post.id && Objects.equals(link, post.link);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hashCode(link);
+        return Objects.hash(id, link);
     }
 }
